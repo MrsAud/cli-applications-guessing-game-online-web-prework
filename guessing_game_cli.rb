716be692b9da0960085f945650109e78da181aae
Audrea Cook <audreacwork@gmail.com>
2019-06-17 18:15:19 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,12 @@
-# Code your solution here!
+def run_guessing_game
+  # generate number 1-6 using rand
+  
+  # prompt user input
+  
+  # capture user input using gets.chomp
+  
+  # print 1 of 3 statements using if (match), elsif (exit), else
+  
+  
+  
+end
